34d8a436078e603d1aa37ff3165a5c014a06de0d
Sumit Kumar <sumitsingh919943@gmail.com>
2026-08-03 23:57:56 +0530

    Time: 7 ms (28.59%) | Memory: 12.8 MB (7.46%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,30 @@
+// class Solution {
+// public:
+//     string intToRoman(int num) {
+        
+//     }
+// };
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        // Mappings of values to Roman numeral symbols in descending order
+        const vector<pair<int, string>> valueSymbols = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
+            {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"},
+            {1, "I"}
+        };
+
+        string result = "";
+        
+        for (const auto& [value, symbol] : valueSymbols) {
+            while (num >= value) {
+                result += symbol;
+                num -= value;
+            }
+        }
+        
+        return result;
+    }
+};
